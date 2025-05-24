78dff48b55d17d7e7a0c53911d44ae7a14c33dcd
Wave <torrent_den2018@mail.ru>
2025-05-24 16:21:48 +0300

    init branch, add db context

---
+++ MusicAga/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicAga.Models
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=musical_equipment.db");
+        }
+    }
+}
